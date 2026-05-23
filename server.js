9ff24b3a1ea06497691267e0239767be68274d59
agcooper20005-design <agcooper20005@gmail.com>
2026-05-23 19:14:00 -0400

    Directory Added

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+//Loadint the environment variables from .env file at the very top
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
 
-app.get('/api/hello',(req, res) => {
-    res.json({
-        message: "Hello World from the Backend!",
-        status: "Alive",
-        timestamp: new Date().toLocaleDateString()
-    })
-})
+//Use process.env to read the PORT variable from hidden .env file
+//If something goes wrong with the file, it defaults to 5000 as a fallback
+const PORT = process.env.PORT || 5000;
 
+//Basic middleware to parse JSON packets incoming from Seth's frontend
+app.use(express.json());
 
+//Simple Test Rout
+app.get('/api/hello', (req, res)=> {
+    res.json({message: "Hello from the MechanicStream CRM Backend!"})
+});
+
+//Startup the server using configured environment port
 app.listen(PORT, () => {
-    console.log('\n Server is running!');
-    console.log(`Link: http://localhost:${PORT}/api/hello`)
+    console.log(`===========================================`);
+    console.log(` Server is running locally on port: ${PORT}`);
+    console.log(` Environment configuration loaded successfully!`);
+    console.log(`===========================================`);
 })
-
